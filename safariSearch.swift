08b7890be0577946e1dc2755892af1351699d5e9
Ahmet Karalar <akaralar@gmail.com>
2016-04-21 04:07:00 +0300

    Add history item struct and read history items correctly

--- safariSearch.swift
+++ safariSearch.swift
@@ -4,6 +4,18 @@ import Foundation
 
 let HISTORY_PATH = "/Caches/Metadata/Safari/History"
 
+struct HistoryItem {
+    let url: NSURL
+    let name: String
+    let fullText: String?
+    
+    init(fromPlist plist: AnyObject) {
+        name = plist.objectForKey("Name") as! String
+        url = NSURL(string: plist.objectForKey("URL") as! String)!
+        fullText = plist.objectForKey("Full Page Text") as? String
+    }
+}
+
 let fileManager = NSFileManager()
 let libraryURL = try! fileManager.URLForDirectory(.LibraryDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: false)
 let fullPath = libraryURL.path!.stringByAppendingString(HISTORY_PATH)
@@ -15,12 +27,10 @@ let historyEnumerator = fileManager.enumeratorAtURL(fullURL, includingProperties
 }
 
 let all = historyEnumerator!.allObjects
+let clipped = all.prefixUpTo(10)
 
-
-//for url in historyEnumerator! {
-//    print(url)
-//}
-//while let currentObject = historyEnumerator?.nextObject() {
-//    print(currentObject)
-//}
-
+for url in clipped {
+    let data = NSData.init(contentsOfURL: url as! NSURL)
+    let plist = try! NSPropertyListSerialization.propertyListWithData(data!, options: .Immutable, format: nil)
+    let item = HistoryItem.init(fromPlist: plist)
+}
