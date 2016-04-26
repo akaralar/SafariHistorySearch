a87a96c45ea5ce1aff8e09cadec079f362f8de25
Ahmet Karalar <akaralar@gmail.com>
2016-04-26 04:16:26 +0300

    show results in alfred

--- SafariHistorySearch.playground/Contents.swift
+++ SafariHistorySearch.playground/Contents.swift
@@ -2,17 +2,70 @@
 import Foundation
 
 let HISTORY_PATH = "/Caches/Metadata/Safari/History"
+let SAFARI_ICON_PATH = "compass.png"
+let MAX_RESULTS = 10
+let EscapeMap = ["&": "&amp",
+                 "\"": "&quot",
+                 "'": "&apos",
+                 "<": "&lt",
+                 ">": "&gt"]
 
 struct HistoryItem {
-    let url: NSURL
-    let name: String
+    let url: NSURL?
+    let name: String?
     let fullText: String?
     
     init(fromPlist plist: AnyObject) {
-        name = plist.objectForKey("Name") as! String
-        url = NSURL(string: plist.objectForKey("URL") as! String)!
+        
+        if let urlString = plist.objectForKey("URL") as? String {
+            url = NSURL(string: urlString)
+        }
+        else {
+            url = nil
+        }
+        name = plist.objectForKey("Name") as? String
         fullText = plist.objectForKey("Full Page Text") as? String
     }
+    
+    func alfredResult() -> AlfredResult? {
+        return url != nil ? AlfredResult(fromHistoryItem: self) : nil
+    }
+}
+
+struct AlfredResult {
+    var uid: String  = ""
+    var arg: String  = ""
+    var title: String  = ""
+    var sub: String  = ""
+    var icon: String  = ""
+    
+    init(fromHistoryItem historyItem: HistoryItem) {
+        let url = historyItem.url!.absoluteString
+        title = historyItem.name ?? "<TITLE_MISSING>"
+        sub = url
+        uid = url
+        arg = url
+        icon = SAFARI_ICON_PATH
+    }
+    
+    func toXML() -> NSXMLElement {
+        let resultXML = NSXMLElement(name: "item")
+        resultXML.addAttribute(NSXMLNode.attributeWithName("uidid", stringValue: uid) as! NSXMLNode)
+        resultXML.addChild(NSXMLNode.elementWithName("arg", stringValue: arg) as! NSXMLNode)
+        resultXML.addChild(NSXMLNode.elementWithName("title", stringValue: title) as! NSXMLNode)
+        resultXML.addChild(NSXMLNode.elementWithName("sub", stringValue: sub) as! NSXMLNode)
+        resultXML.addChild(NSXMLNode.elementWithName("icon", stringValue: icon) as! NSXMLNode)
+        return resultXML
+    }
+    
+}
+
+func escapedString(unescaped: String) -> String {
+    var escaped = unescaped
+    for (key, value) in EscapeMap {
+        escaped = escaped.stringByReplacingOccurrencesOfString(key, withString: value)
+    }
+    return escaped
 }
 
 let fileManager = NSFileManager()
@@ -28,12 +81,21 @@ let historyEnumerator = fileManager.enumeratorAtURL(fullURL, includingProperties
 let all = historyEnumerator!.allObjects
 let clipped = all.prefixUpTo(10)
 
+var results = [AlfredResult]()
+let root = NSXMLElement(name: "items")
 for url in clipped {
     let data = NSData.init(contentsOfURL: url as! NSURL)
     let plist = try! NSPropertyListSerialization.propertyListWithData(data!, options: .Immutable, format: nil)
-    let item = HistoryItem.init(fromPlist: plist)
+    let item = HistoryItem(fromPlist: plist)
+    
+    guard let alfredResult = item.alfredResult() else {
+        continue
+    }
+    
+    results.append(alfredResult)
+    let resultXML = alfredResult.toXML()
+    root.addChild(resultXML)
 }
 
-
-
-
+var xml = NSXMLDocument(rootElement: root)
+print(xml.XMLStringWithOptions(NSXMLNodePrettyPrint))
